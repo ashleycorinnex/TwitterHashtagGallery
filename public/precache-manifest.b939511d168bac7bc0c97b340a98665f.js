530b4c0713562ccff4905437781d52e0abc3e1ff
Ashley McBride <ashleycorinnex@gmail.com>
2019-04-20 13:21:16 -0700

    test public

---
+++ public/precache-manifest.b939511d168bac7bc0c97b340a98665f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "6a386899746222073dd64c5f74d1a69d",
+    "url": "/static/media/ProximaNovaSemibold.6a386899.otf"
+  },
+  {
+    "revision": "410504d49238e955ba7dc23a7f963021",
+    "url": "/static/media/ProximaNovaRegular.410504d4.otf"
+  },
+  {
+    "revision": "72b6f2c9990bd3d37b8013a59ed78902",
+    "url": "/static/media/ProximaNovaBold.72b6f2c9.otf"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "998c27f0818f496c3614",
+    "url": "/static/js/main.648b7073.chunk.js"
+  },
+  {
+    "revision": "16cee2f138af497c970d",
+    "url": "/static/js/2.61ebbd2e.chunk.js"
+  },
+  {
+    "revision": "998c27f0818f496c3614",
+    "url": "/static/css/main.89556789.chunk.css"
+  },
+  {
+    "revision": "74bf148533b2cd115d6249d06e12ab33",
+    "url": "/index.html"
+  }
+];
